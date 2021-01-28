f6ab91b6a12d75c50e2ff5ea4c08d66f84f0095c
laulaiu <78058047+laulaiu@users.noreply.github.com>
2021-01-27 23:31:43 -0300

    Add files via upload

---
+++ Controle de vendade estoque/src/Classes/Produtos.java
@@ -0,0 +1,42 @@
+package Classes;
+
+
+public class Produtos {
+	private int id;
+	private int quantidade;	
+	private String nome;
+	private boolean preco;
+	
+	
+	public int getId() {
+		return id;
+		
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public boolean isPreco() {
+		return preco;
+	}
+	public void setPreco(boolean preco) {
+		this.preco = preco;
+	}
+	
+	
+		
+	
+	
+
+}
---
+++ Controle de vendade estoque/src/menu/Menu.java
@@ -0,0 +1,33 @@
+package menu;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		int opcao;
+		
+		System.out.println("-----------------------------");
+		System.out.println(" Controle de venda de estoque");
+		System.out.println("------------------------------");
+		System.out.println("");
+		System.out.println("[1] Cadastrar produtos");
+		System.out.println("[2] Consultar prosutos cadastrados");
+		System.out.println("[3] Listas todos os produtos");
+		System.out.println("[4] Editar produto");
+		System.out.println("[5] Sair");
+		
+		Scanner sc = new Scanner(System.in);
+		opcao = sc.nextInt();
+		
+		
+		
+		
+		
+		
+
+
+	}
+
+}
