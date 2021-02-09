871144194a54e2cc55e3a6faa901d7957be20d47
Leonardo <Leonardo@192.168.1.13>
2021-02-08 21:54:11 -0300

    terminei o Projeto adicionando o restante dos menus e atualizando alguns codigos para a correção de Bugs

--- Controle de vendade estoque/src/Classes/Produtos.java
+++ Controle de vendade estoque/src/Classes/Produtos.java
@@ -1,6 +1,15 @@
 package Classes;
 
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+
 
 public class Produtos {
 	
@@ -41,29 +50,284 @@ public class Produtos {
 		this.nome = nome;
 		this.preco = preco;
 		this.quantidade = quantidade;
-		
+
 		 
 	}
 	
+	public boolean gravaArquivo(boolean testeboolean, boolean falso) {
+		
+		FileOutputStream arquivo = null;
+		
+		try{
+			
+			arquivo = new FileOutputStream("cadastro_Produto.txt", testeboolean);
+			
+			if((!nome.equals("") ) && (id != 0)) {
+				
+				String inf = (id+";"+nome+";"+preco+";"+quantidade+"\r\n");
+				
+				byte[] informacao = inf.getBytes();
+				
+				arquivo.write(informacao);
+				
+				falso = true;
+			}
+			
+
+			
+			
+			
+		}catch(IOException e){
+			System.out.println("**falha ao gravar arquivo**");
+			
+		}
+		return falso;
+	}
 	
-//	public void gravarInfo(String juncaoProdutos) {
-//		FileOutputStream documento = null;
-//		
-//		juncaoProdutos = this.id + ";" + this.nome +";" + this.preco +";" + this.quantidade  ;
-//		 try {
-//			 documento= new FileOutputStream("cadastro_Produtos.txt", true);
-//			  
-//			 byte[] produtoByte = juncaoProdutos.getBytes();
-//			  
-//			 documento.write(produtoByte);
-//
-//		 }catch(Exception e) {
-//			 System.out.println("erro inesperado! ");
-//		 }
-//		  
-//		 System.out.println("Cadastro Realizado");
-//		
-//	}
+	
+	
+	//verifica se o nome ja esta cadastrado
+	public boolean lerArquivo( String nomeProd) {
+
+		boolean retorno = false;
+		
+		FileInputStream arquivo = null;
+		InputStreamReader lerArquivo = null;
+		BufferedReader convertArquivo = null;
+		
+		try {
+			
+			arquivo = new FileInputStream("cadastro_Produto.txt");
+			lerArquivo = new InputStreamReader(arquivo);
+			convertArquivo = new BufferedReader(lerArquivo);
+			
+			while(convertArquivo.ready()) {
+				
+				String linha = convertArquivo.readLine();
+				String[] infoproduto = linha.split(";");
+				
+				if(infoproduto[1].equals(nomeProd)) { 
+					
+					retorno = true;
+					break;
+				}
+			}
+
+		}catch(IOException e) {
+			System.out.println("Não foi possível ler o arquivo!");
+			System.out.println("criando arquivo novo...");
+		}
+	
+		return retorno;
+	}
+	
+	
+	
+	//verifica se o id ja esta cadastrado
+	public boolean lerId( int idProd) {
+
+		boolean retorno = false;
+		
+		FileInputStream arquivo = null;
+		InputStreamReader lerArquivo = null;
+		BufferedReader convertArquivo = null;
+		
+		try {
+			
+			arquivo = new FileInputStream("cadastro_Produto.txt");
+			lerArquivo = new InputStreamReader(arquivo);
+			convertArquivo = new BufferedReader(lerArquivo);
+			
+			while(convertArquivo.ready()) {
+				
+				String linha = convertArquivo.readLine();
+				String[] infoproduto = linha.split(";");
+				
+				if( Integer.parseInt(infoproduto[0]) ==  idProd) { 
+					
+					retorno = true;
+					break;
+				}
+			}
+
+		}catch(Exception e) {
+			System.out.println("Não foi possível ler o arquivo da ID! ");
+			
+		}
+
+		return retorno;
+	}
+	
+	
+	//Busca pelo nome ou id
+		public Boolean Buscaproduto( String nomeouid, boolean falso) {	
+			
+			FileInputStream arquivo = null;
+			InputStreamReader lerArquivo = null;
+			BufferedReader convertArquivo = null;
+			
+			try {
+				
+				arquivo = new FileInputStream("cadastro_Produto.txt");
+				lerArquivo = new InputStreamReader(arquivo);
+				convertArquivo = new BufferedReader(lerArquivo);
+				
+				while(convertArquivo.ready()) {
+					
+					String linha = convertArquivo.readLine();
+					String[] infoproduto = linha.split(";");
+					
+					if(( infoproduto[0].equals(nomeouid))||(infoproduto[1].equals(nomeouid))) { 
+						System.out.println("\r\n");
+						System.out.println("Id: "+infoproduto[0]);
+						System.out.println("Nome: "+infoproduto[1]);
+						System.out.println("Preco: "+infoproduto[2]);
+						System.out.println("Quantidade: "+infoproduto[3]);
+						System.out.println("\r\n");
+						falso = true;
+
+					}
+				}
+
+			}catch(IOException e) {
+				System.out.println("Não foi possível Buscar o produto! ");
+			}
+			return falso;
+			
+		}
+		
+		
+		//Imprime Todos os produtos
+		public void listaProdutos( ) {	
+			
+			FileInputStream arquivo = null;
+			InputStreamReader lerArquivo = null;
+			BufferedReader convertArquivo = null;
+			
+			try {
+				
+				arquivo = new FileInputStream("cadastro_Produto.txt");
+				lerArquivo = new InputStreamReader(arquivo);
+				convertArquivo = new BufferedReader(lerArquivo);
+				
+				while(convertArquivo.ready()) {
+					
+					String linha = convertArquivo.readLine();
+					String[] infoproduto = linha.split(";");
+					System.out.println("\r\n");
+					System.out.println("Id: "+infoproduto[0]);
+					System.out.println("Nome: "+infoproduto[1]);
+					System.out.println("Preco: "+infoproduto[2]);
+					System.out.println("Quantidade: "+infoproduto[3]);
+					System.out.println("\r\n");
+				}
+
+			}catch(IOException e) {
+				System.out.println("Não foi possível listar todos produto! ");
+			}
+			
+		}
+		
+		
+		//atualiza lista do arquivo.txt
+		public void atualizaArquivo( ) {	
+			
+			
+			FileInputStream arquivo = null;
+			InputStreamReader lerArquivo = null;
+			BufferedReader convertArquivo = null;
+			
+			try {
+				
+				arquivo = new FileInputStream("cadastro_Produto.txt");
+				lerArquivo = new InputStreamReader(arquivo);
+				convertArquivo = new BufferedReader(lerArquivo);
+				
+				while(convertArquivo.ready()) {
+					
+					String linha = convertArquivo.readLine();
+					String[] infoproduto = linha.split(";");
+					System.out.println("\r\n");
+					System.out.println("Id: "+infoproduto[0]);
+					System.out.println("Nome: "+infoproduto[1]);
+					System.out.println("Preco: "+infoproduto[2]);
+					System.out.println("Quantidade: "+infoproduto[3]);
+					System.out.println("\r\n");
+					
+
+					
+				}
+
+			}catch(IOException e) {
+				System.out.println("Não foi possível listar todos produto! ");
+			}
+			
+		}
+		
+		//editar  produto
+		public boolean editaproduto( String nomeouid) {
+			
+			boolean vdd = false;
+			
+			Scanner sc = new Scanner(System.in);
+			FileInputStream arquivo = null;
+			InputStreamReader lerArquivo = null;
+			BufferedReader convertArquivo = null;
+			
+			try {
+				
+				arquivo = new FileInputStream("cadastro_Produto.txt");
+				lerArquivo = new InputStreamReader(arquivo);
+				convertArquivo = new BufferedReader(lerArquivo);
+				
+				while(convertArquivo.ready()) {
+					
+					String linha = convertArquivo.readLine();
+					String[] infoproduto = linha.split(";");
+					
+					if(( infoproduto[0].equals(nomeouid))||(infoproduto[1].equals(nomeouid))) { 
+						System.out.println("\r\n");
+						System.out.println("Id: "+infoproduto[0]);
+						System.out.println("Nome: "+infoproduto[1]);
+						System.out.println("Preco: "+infoproduto[2]);
+						System.out.println("Quantidade: "+infoproduto[3]);
+						System.out.println("\r\n");
+						
+						System.out.println("deseja editar ? [1]sim [2]não");
+						int decisao = sc.nextInt();
+						
+						
+						if(decisao == 1) {
+							
+							vdd = true;
+						}
+
+					}else {
+						if(vdd == false) {
+							vdd = false;
+						}
+					}
+				}
+
+			}catch(IOException e) {
+				System.out.println("Não foi possível Buscar o produto! ");
+			}
+			
+			return vdd;
+			
+		}
+		
+
+
+
+		
+		
+
+		
+		
+		
+
+
 	
 	
 		
--- Controle de vendade estoque/src/menu/Menu.java
+++ Controle de vendade estoque/src/menu/Menu.java
@@ -1,33 +1,35 @@
 package menu;
 
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
-
 import java.util.Scanner;
+
 import Classes.Produtos;
 
 public class Menu {
 
 	public static void main(String[] args) {
-
-		Produtos[] produto = new Produtos[100];
+		
+		
+		//cria um vetor de Objetos da classe Produtos
+		Produtos[] produto = new Produtos[10];
 
 		for (int i = 0; i < produto.length; i++) {
 			produto[i] = new Produtos();
+			produto[i].setNome("");
+			
 		}
+		
+		
 
 		Random aleatorio = new Random();
 		int numeros = aleatorio.nextInt(100);
 		
-		Scanner sc = null;
-		
-		//Manipula arquivo txt
-		FileInputStream  consultaArquivo = null;
-		InputStreamReader lendoArquivo = null;
-		BufferedReader  convertendoArquivo = null;
+		Scanner sc = null;		
 		
 		
 		//Atributos dos Produtos
@@ -37,16 +39,12 @@ public class Menu {
 		String nome;
 		
 		//variaveis para a construçao do programa
-		String consultaProduto;
-		int decisao = 0;
+		int numVetor;
 		int opcao = 0;
-		int numVetor= 0;
-		int verificaId = 1 ;
-		
-		
+
 
 		while (opcao != 5) {
-
+			
 			System.out.println("-----------------------------");
 			System.out.println(" Controle de venda de estoque");
 			System.out.println("-----------------------------");
@@ -63,11 +61,10 @@ public class Menu {
 
 			
 			if (opcao == 1) {
+				
 				  System.out.println("-----------------------------"); 
 				  System.out.println(" Cadastro de produtos"); 
 				  System.out.println("-----------------------------");
-				  	
-
 				  
 				  System.out.println("");
 				  
@@ -82,160 +79,217 @@ public class Menu {
 				  System.out.println("Digite a quantidade de produtos"); 
 				  quantidade = sc.nextInt();
 				  
-				  System.out.println("Deseja salvar o cadastro ? [1=sim] [0=nao]");
-				  decisao = sc.nextInt();
 				  
-				  FileOutputStream documento = null;
+					
+				  // Verifica se o nome do produto ja foi cadastrado				  
+			  	  boolean  vdd = true ;
+				  while(vdd == true) {
 				  
-				  for(numVetor = 0; numVetor <= produto.length; numVetor++) {
-					  if(produto[numVetor].getNome() == null) {
+				  vdd = produto[1].lerArquivo(nome);				  
+					  if( vdd == true) {					  
+						  System.out.println("Produto já cadastrado");					  
+						  System.out.println("Digite o nome do produto");
+						  nome = sc.next();
+					  
+					  } 
+				  }
+					  
+				  
+					id = 59;  
+				  //verifica se o id do produto ja foi cadastrado 
+				  vdd = true; 
+				  while(vdd == true) {
+				  
+					vdd = produto[1].lerId(id);
+				  	if(vdd == true) {
+				  		id = numeros = aleatorio.nextInt(100);; 
+				  	}
+				  
+				  }
+					   
+				  
+				  
+				  
+			  for(numVetor = 0; numVetor<produto.length; numVetor++) {
+					  
+					  if((produto[numVetor].getNome() == "") && (produto[numVetor].getId() == 0)){					
 						  
+						  produto[numVetor].setAll(id, nome, preco, quantidade);
+						  produto[numVetor].gravaArquivo(true,false);
+						  System.out.println("Gravado com sucesso!");
 						  break;
-						 
+						  
+					  }
+					  
+			  }
+		  
+			}// Fim do if 1
+			
+			
+			
+			
+			if(opcao == 2) {
+				
+				  System.out.println("-----------------------------"); 
+				  System.out.println(" Busca de produtos"); 
+				  System.out.println("-----------------------------");
+				  
+				  System.out.println("");
+				  
+				  System.out.println("Digite o nome do produto ou a id"); 
+				  String nomeOuId = sc.next();
+				  
+				  boolean falso = false;
+				  
+				  falso = produto[1].Buscaproduto(nomeOuId, falso);
+				  
+				  if(falso == false ) {
+					  System.out.println("Produto não cadastrado!");
+				  }
+				  
+				  
+				  	
+				
+			}
+			
+			
+			
+			
+			if(opcao == 3) {
+				
+				  System.out.println("-----------------------------"); 
+				  System.out.println(" Lista todos os produtos     "); 
+				  System.out.println("-----------------------------");
+				  
+				  System.out.println("");
+				  
+				  produto[1].listaProdutos();
+				  
+				
+				
+			}
+			
+			if(opcao == 4) {
+				
+				  System.out.println("-----------------------------"); 
+				  System.out.println(" Editar Produtos    "); 
+				  System.out.println("-----------------------------");
+				  
+				  System.out.println("");
+				  
+				  System.out.println("Digite o nome do produto ou a id"); 
+				  String nomeid = sc.next();
+				  
+				  boolean verdadeiro = produto[0].editaproduto(nomeid);
+				  
+				  if(verdadeiro == true) {
+					  					  
+							FileInputStream arquivo = null;
+							InputStreamReader lerArquivo = null;
+							BufferedReader convertArquivo = null;
+							
+							try {
+								
+								arquivo = new FileInputStream("cadastro_Produto.txt");
+								lerArquivo = new InputStreamReader(arquivo);
+								convertArquivo = new BufferedReader(lerArquivo);
+								
+								int n1 = 0;
+								
+								while(convertArquivo.ready()) {
+									
+									String linha = convertArquivo.readLine();
+									String[] infoproduto = linha.split(";");
+									
+									
+									int idconvertido = Integer.parseInt(infoproduto[0]);
+									String nomeconvertido = infoproduto[1];
+									double precoconvertido = Double.parseDouble(infoproduto[2]);
+									int quantidadeconvertido = Integer.parseInt(infoproduto[3]);
+									
+									produto[n1].setAll(idconvertido, nomeconvertido,
+											precoconvertido,quantidadeconvertido );
+									
+									n1 = n1 + 1;
+									
+
+									
+								}
+
+							}catch(IOException e) {
+								System.out.println("Não foi possível listar todos produto! ");
+							}
+						  
+						  
+					  
+					  
+				  }// fim do if vdd
+				  
+				  if(verdadeiro == true) {
+					  
+				  
+				  
+				  for( int Vetor = 0; Vetor < produto.length; Vetor++) {
+					  if((produto[Vetor].getNome().equals(nomeid))||
+							  (Integer.toString(produto[Vetor].getId()).equals(nomeid))) {
+						  
+						  System.out.println("nome do edit: "+produto[Vetor].getNome());
+						  System.out.println("Digite um novo nome: ");
+						  String nome2 = sc.next();
+						  
+						  System.out.println("Digite um preço: ");
+						  double preco2 = sc.nextDouble();
+						  
+						  System.out.println("Digite a quantidade: ");
+						  int quantidade2 = sc.nextInt();
+						  
+						  
+						  produto[Vetor].setNome(nome2);
+						  produto[Vetor].setPreco(preco2);
+						  produto[Vetor].setQuantidade(quantidade2);
+					  
+						  
 					  }
 					  
 				  }
-				  
-				 if ((decisao == 1 )&&(produto[numVetor].getNome() == null ) ) {			
-					 	 
-						
-						 try {
 
-								//*************compara****************
-								try {									
-									
-									consultaArquivo = new FileInputStream("cadastro_Produtos.txt");
-									lendoArquivo = new InputStreamReader(consultaArquivo);
-									convertendoArquivo = new BufferedReader(lendoArquivo);
-
-									
-									
-										id = 24;
-										
-										String passagem = convertendoArquivo.readLine();
-										String[] informacao = passagem.split(";");
-										
-
-										while(verificaId == 1) {
-											
-											while(convertendoArquivo.ready()) {
-												
-												//verifica se o id ja foi cadastrado;
-																	
-												if(informacao[0].equals( String.valueOf(id) )) {
-													System.out.println("id ja existe");
-													id = numeros;
-													verificaId = 1;
-													
-												}else {
-													System.out.println("Id disponivel");
-													verificaId = 0;
-												}
-												
-												
-												
-												//verifica se o nome ja foi cadastrado
-												if((informacao[1].equals(nome))) {
-													
-													System.out.println("Produto já cadastrado");
-													System.out.println("Digite o nome do produto"); 
-													nome = sc.next();
-													
-											
-													
-												}else {
-													
-													 System.out.println("entro no try catch");
-													 produto[numVetor].setNome(nome);
-													 produto[numVetor].setPreco(preco);
-													 produto[numVetor].setQuantidade(quantidade);
-													 
-												}
-											}
-										}
-									
-									
-								}catch(Exception e) {
-									System.out.println("Erro inesperado");
-								}
-							 
-							 produto[numVetor].setId(id); 
-							 
-							 String juncaoProdutos = id + ";" + nome +";" + preco +";" + quantidade +  "\r\n"  ;
-							 System.out.println(juncaoProdutos);
-							 
-							 documento= new FileOutputStream("cadastro_Produtos.txt", true);
-							 						 
-						  
-							 byte[] produtoByte = juncaoProdutos.getBytes();
-						  
-							 documento.write(produtoByte);
-						  
-						  }catch(Exception e) { 
-							  System.out.println("erro inesperado! "); 
-						  }
-						 
-					  
-						 System.out.println("Cadastro Realizado"); 
-				 }else {
-					 System.out.println("Cadastro pedido");
-				 }					
+				boolean gravaVdd= false;
+				boolean falso = false;
 				
-			}// fim do if opcao 1
-			
-			
-			
-	
-			
-			
-			//consultando produto no arquivo txt
-			
-			if (opcao == 2) {
-				System.out.println("-----------------------------");
-				System.out.println("  Consulta de Produto    ");
-				System.out.println("-----------------------------");
-
-				System.out.println("Digite o nome do produto ou ID");
-				consultaProduto = sc.next();
-				
-
-				
-				//*************busca o Produto****************
-				try {									
+				for(int Vetor = 0; Vetor <produto.length;Vetor++){
 					
-					consultaArquivo = new FileInputStream("cadastro_Produtos.txt");
-					lendoArquivo = new InputStreamReader(consultaArquivo);
-					convertendoArquivo = new BufferedReader(lendoArquivo);
-
-					
-					while(convertendoArquivo.ready()) {
-						
-						
-						String passagem = convertendoArquivo.readLine();
-						String[] informacao = passagem.split(";");
-						
-						if((informacao[0].equals(consultaProduto)) || (informacao[1].equals(consultaProduto)))  {
-							
-							System.out.println("\r\n");
-							System.out.println("ID: "+informacao[0]);
-							System.out.println("Nome: "+informacao[1]);
-							System.out.println("Preco: "+informacao[2]);
-							System.out.println("Quantidade: "+informacao[3]);
-							System.out.println("\r\n");
-							
-						}
-				
+					if(Vetor == 1) {
+						gravaVdd = true;						
 					}
 					
-				}catch(Exception e) {
-					System.out.println("Erro inesperado");
+					falso = produto[Vetor].gravaArquivo(gravaVdd, falso);
+					
+
+					
+					
 				}
 				
-
-
-			} // fim if opcao 2
+				if(falso == true) {
+					System.out.println("Editado com sucesso");	
+				}else {
+					System.out.println("Falha ao editar;");
+				}
+				
+				}else {
+					System.out.println("Produto cancelado ou nao encontrado!");
+				}
+				
+				
+			}//fim do if 4
+			
+			
 
 		} // fim do while
+		
+		sc.close();
+		
+		System.out.println("\r\n");
+		System.out.println("*Fim do Programa*");
 
 	}// fim do metodo main
 
